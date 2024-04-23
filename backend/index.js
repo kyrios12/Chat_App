a19306e1c2e31c1bfc8e661d10029446405ad6ad
Abdul Azeez <103828615+kyrios12@users.noreply.github.com>
2024-04-23 16:34:24 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const server = require("http").createServer(app);
 app.use(cors());
 const io = new Server(server, {
   cors: {
-    origin: "https://chat-app-mysz.vercel.app/",
+    origin: "http://localhost:3000/",
     methods: ["GET", "POST"],
   },
 });
